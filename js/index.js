353d7f856af8893fde3c25e5e27fe3dec5eb9f32
Operapond <pondatikan01@gmail.com>
2021-08-29 14:11:44 +0700

    project

---
+++ js/index.js
@@ -0,0 +1,67 @@
+//Home
+const getProduct = async () => {
+  try {
+      const response = await axios.get(
+          "https://6102d7aa79ed680017482359.mockapi.io/productlist"
+      );
+      document.getElementById("allProductCard").innerHTML = response.data
+          .map(
+              (product) =>
+                  `
+              <div class="col-lg-3 col-md-4 col-6  flex-wrap style="margin: 0 auto;">
+                  <div class="card card-page1">
+                      <img src="${product.prdImageUrl}" class="card-img-top" alt="pic1">
+                      <div class="card-body">
+                          <h8 class="card-title">${product.prdname} </h8>
+                          <div class="row card-price-btn">
+                              <div class="col">
+                                  <p class="card-text">${product.prdPrice}THB</p>
+                              </div>
+                              <div class="col">
+                                  <button onclick="location.href = 'product.html?id=${product.id}';" type="button " class="btn btn-dark btn-addCart">Add</button>
+                              </div>
+                              </div>
+                      </div>
+                  </div>
+              </div>
+
+          `
+          )
+          .join("");
+  } catch (e) {
+      console.log(`e`, e);
+  }
+};
+
+//Slide baner
+const activeImage = document.querySelector(".carou-image-ac");
+const activeText = document.querySelector(".active-text");
+const getCarousel = async () => {
+  try {
+      const response = await axios.get(
+          "https://6102d7aa79ed680017482359.mockapi.io/slider"
+      );
+      activeImage.src = response.data[3].imageUrl;
+      activeText.innerHTML = response.data[3].sliderText;
+      //console.log(response.data[0])
+      document.getElementById("carouselImage").innerHTML = response.data
+      document.getElementById("carouselImage").innerHTML = response.data
+          .map(
+              (imageCarou) =>
+
+                  `
+          <div class="carousel-item ">
+              <img src="${imageCarou.imageUrl}" class="d-block" alt="...">
+                  <div class="carousel-caption d-block">
+                      <p>${imageCarou.sliderText}</p>
+                  </div>
+          </div>
+          
+          ${console.log(imageCarou.sliderText)}
+          `
+          )
+          .join("");
+  } catch (e) {
+      console.log(`e`, e);
+  }
+}
---
+++ js/product.js
@@ -0,0 +1,97 @@
+//select product
+let productid = new URLSearchParams(window.location.search).get("id");
+console.log(`productid`, productid);
+
+const getProductdetail = async () => {
+    try {
+        const response = await axios.get(
+            `https://6102d7aa79ed680017482359.mockapi.io/productdetail?id=${productid}`
+        );
+        document.getElementById("photopro").innerHTML = response.data.map(
+            (product, index) => {
+                if (index === 0) {
+                    return `
+                    <div class="row">
+                        <div class=" col-md-7 col-sm-12">
+                        <div class="row">
+                            <img src="${product.prdImageUrl}" class="photoproduct">
+                        </div>
+                        </div>
+                         <div class=" detail col-md-4">
+                            <h3>${product.prdname}</p> </h3><br>
+                            <h5 class="price-product">${product.prdPrice}THB</h5><br>
+                            <div class="detailleft row g-3">
+                                <div class="col-md-12" >
+                                <div class="row g-3">
+                                     <label for="inputEmail4" class="form-label">Size</label>
+                                    <div class="col-md-12  col-sm-6 col-6">
+                                       
+                                        <select class="form-select" id="selectSize" aria-label="Default select example">
+                                            <option selected >Please Select</option>
+                                            ${product.prdSize.map((product) => {
+                                                return `<option value ="${product}">${product}</option>`
+                                            })}
+                                        </select>
+                                    </div>
+                                    <div class="col-md-12 col-sm-6 col-6 ">
+                                    
+                                    <button class="btn btn-dark addtb" onclick="addItem('${productid}','${product.prdname}','${product.prdPrice}'
+                                    ,'${product.prdSize.map(product => `${product}`).join('')}','${product.prdImageUrl}','${product.txtDetail}')";   
+                                    type="button "  id ="add_productSize">Add To Bag</button>
+                                    </div>
+                                    </div>
+                                </div>
+                                <h2>Product detail</h2>
+                                <p>${product.txtDetail}</p><br>
+                                <button class="showbtn">Show More Detail</button><br>
+                            </div>
+                        </div>
+        
+                    </div>
+                    `
+                }
+            }
+
+
+        )
+            .join("");
+
+    } catch (e) {
+        console.log(`e`, e);
+    }
+};
+//เก็บค่าSize+ข้อมูลรองเท้า+เปลี่ยนหน้า
+function addItem(id, name, price, size, image, detail) {
+    if (localStorage.getItem('product')) {
+        var oldItems = JSON.parse(localStorage.getItem('product'));
+        let selectSize = document.getElementById("selectSize").value;
+        const arraySize = size.split('')
+        var newItem = {
+            "id": id,
+            "prdname": name,
+            "prdPrice": price,
+            "prdSize": selectSize,
+            "size": arraySize,
+            "prdImageUrl": image,
+            "txtDetail": detail
+        };
+        let finItem = oldItems.concat(newItem);
+        localStorage.setItem('product', JSON.stringify(finItem));
+        location.href = 'shoppingcart.html';
+    } else {
+        let selectSize = document.getElementById("selectSize").value;
+        const arraySize = size.split('')
+        var newItem = [{
+            "id": id,
+            "prdname": name,
+            "prdPrice": price,
+            "prdSize": selectSize,
+            "size": arraySize,
+            "prdImageUrl": image,
+            "txtDetail": detail
+        }];
+        localStorage.setItem('product', JSON.stringify(newItem));
+        location.href = 'shoppingcart.html';
+    }
+};
+getProductdetail();
